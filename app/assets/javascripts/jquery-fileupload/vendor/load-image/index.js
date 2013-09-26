45e8eba3ff40753498b7b62a5b599bfc32a93286
Alexey Plutalov <demiazz.py@gmail.com>
2013-09-26 09:42:33 +0400

    Update all libraries. Add full version of load-image

---
+++ app/assets/javascripts/jquery-fileupload/vendor/load-image/index.js
@@ -0,0 +1,8 @@
+/*
+ *= require ./load-image
+ *= require ./load-image-orientation
+ *= require ./load-image-meta
+ *= require ./load-image-ios
+ *= require ./load-image-exif
+ *= require ./load-image-exif-map
+ */
